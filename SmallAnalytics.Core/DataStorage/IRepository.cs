b9e832aaf3671309b8da3cc36d68239ab8bdbc31
Andrey Kniazev <worldpwn@hotmail.com>
2020-02-26 20:46:25 +0300

    added repository interface

---
+++ SmallAnalytics.Core/DataStorage/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using SmallAnalytics.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallAnalytics.Core.DataStorage
+{
+    public interface IRepository
+    {
+        Task AddAsync(IEnumerable<AnalyticsDataDTO> analyticsDataDTOs);
+    }
+}
