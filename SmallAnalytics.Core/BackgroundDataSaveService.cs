b76a22ee4935306d9870db9767b30ae5bf9e5a10
Andrey Knyazev <worldpwn@hotmail.com>
2020-02-28 15:50:10 +0300

    added separated hosted service

---
+++ SmallAnalytics.Core/BackgroundDataSaveService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmallAnalytics.Core
+{
+    public class BackgroundDataSaveService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Saving what is left in queue
+            throw new NotImplementedException();
+        }
+    }
+}
