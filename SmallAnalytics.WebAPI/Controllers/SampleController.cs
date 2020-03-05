505db37ddd69ed7b94fcb3c1f1f06343b30c9a6e
Andrey Knyazev <worldpwn@hotmail.com>
2020-03-05 16:49:27 +0300

    finished with jmetre

--- SmallAnalytics.WebAPI/Controllers/SampleController.cs
+++ SmallAnalytics.WebAPI/Controllers/SampleController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using SmallAnalytics.Core;
 using SmallAnalytics.MsSQL;
 using SmallAnalytics.MsSQL.Modesl;
+using SmallAnalytics.WebAPI.ViewModels;
 
 namespace SmallAnalytics.WebAPI.Controllers
 {
@@ -35,10 +36,16 @@ namespace SmallAnalytics.WebAPI.Controllers
             return await _context.AnalyticsDatas.ToListAsync();
         }
 
-        [HttpPost]
-        public ActionResult Post(string data)
+        [HttpGet("Count")]
+        public async Task<int> Count()
         {
-            _queue.AddToQueue(new AnalyticsDataModel(DateTimeOffset.UtcNow, data));
+            return await _context.AnalyticsDatas.CountAsync();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]DataViewModel data)
+        {
+            _queue.AddToQueue(new AnalyticsDataModel(DateTimeOffset.UtcNow, data.Content));
             _logger.LogInformation("adding data to queue", data);
             return Ok();
         }
